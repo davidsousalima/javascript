896163d84a67841362c463233cf54789aaf0ebf6
davidsousalima <davidsousamusica01@gmail.com>
2022-08-28 20:45:01 -0300

    Update

--- desafios/d013/script.js
+++ desafios/d013/script.js
@@ -7,14 +7,18 @@ function verificar(){
     if(media >= 6){
         res.innerHTML += `<h2>Analisando a situação de ${nome}</h2>`
         res.innerHTML += `<p>Com as notas ${primeira_nota} e ${segunda_nota} a média e ${media}</p>`
-        res.innerHTML += `<p>Com a media acima de 6,0 o aluno está APROVADO</p>`
+        res.innerHTML += `<p>Com a media acima de 6,0 o aluno está <mark id='verd'>APROVADO</mark></p>`
     }else {
         if(media >= 3 && media < 6){
-            
+            res.innerHTML += `<h2>Analisando a situação de ${nome}</h2>`
+            res.innerHTML += `<p>Com as notas ${primeira_nota} e ${segunda_nota} a média e ${media}</p>`
+            res.innerHTML += `<p>Com a média entre 3,0 e 6,0 o aluno está em <mark id='amar'>RECUPERAÇÂO</mark></p>`
+        }else{
+            res.innerHTML += `<h2>Analisando a situação de ${nome}</h2>`
+            res.innerHTML += `<p>Com a as notas ${primeira_nota} e ${segunda_nota} a média e ${media}</p>`
+            res.innerHTML +=`<p>Com as notas abaixo de 6,0 o aluno esta <div id='verm'>REPROVADO</div></p>`
         }
-        res.innerHTML += `<h2>Analisando a situação de ${nome}</h2>`
-        res.innerHTML += `<p>Com as notas ${primeira_nota} e ${segunda_nota} a média e ${media}</p>`
-        res.innerHTML += `<p>Com a média entre 3,0 e 6,0 o aluno está em RECUPERAÇÂO</p>`
+        
 
     }
 }
